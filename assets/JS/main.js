a63506028bf03cb6eaff59c6639d58daf8ca6c83
Vincepan47 <79616694+Vincepan47@users.noreply.github.com>
2021-06-20 18:52:02 +0800

    Add files via upload

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,24 +2,6 @@ window.addEventListener("scroll", function(){
             var nav = document.querySelector("nav");
             nav.classList.toggle("sticky",window.scrollY > 0);
         })
-        
-        //hoverbtn//
-        var hoverbutton = document.getElementById("hoverbtn");
-
-        window.onscroll = function() {scrollFunction()};
-
-        function scrollFunction() {
-        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-            hoverbutton.style.display = "block";
-        } else {
-            hoverbutton.style.display = "none";
-        }
-        }
-
-        function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-        }
 
         function burgerFunction(x){
             x.classList.toggle("change");
